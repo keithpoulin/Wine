d177f50d26db153829ae8a24c3971b7e82bc0f1f
Neil Poulin <neil.j.poulin@gmail.com>
2012-10-28 20:23:53 -0600

    Added some new UI stuff - styles, etc. Added Gson to the Lib Project directory.

--- WineCellar/WebContent/js/main.js
+++ WineCellar/WebContent/js/main.js
@@ -1,14 +1,36 @@
 $(document).ready(function(){
-	$("#getVineyards").click(function(){
-		getVineyards(null);
-	});
+	initializeEvents();
+	initializeAppearance();
 });
+
+function initializeEvents(){
+	$("#getVineyards").click(function(){
+		$("#vineyards").slideUp(200, function(){
+			getVineyards(null);
+		});		
+	});
+}
+
+function initializeAppearance(){
+	$("#getVineyards").button();
+}
+
+function getArticles(args){
+	$.ajax({
+		url: "http://www.benzawacki.com/article.jsp?num=all&index=0&dir=ascending",
+		data: args,
+		success: function(resp){
+			$("#vineyards").html(resp);
+		}
+	});
+}
 
 function getVineyards(args){
 	$.ajax({
 		url: "/getVineyards",
 		data: args,
 		success: function(resp){
+			$vineyards = $("#vineyards");						
 			displayVineyards(resp);
 		}
 	});
@@ -16,4 +38,5 @@ function getVineyards(args){
 
 function displayVineyards(vineyards){
 	$("#vineyards").html(vineyards);
+	$vineyards.slideDown(500);
 }
--- WineCellar/WebContent/pages/main.jsp
+++ WineCellar/WebContent/pages/main.jsp
@@ -2,15 +2,17 @@
 <html>
 	<head>
 		<link rel="stylesheet" href="/styles/main.css" />
+		<link rel="stylesheet" href="/styles/jquery-ui-active/jquery-ui.css" />
 	</head>
 	
 	<body>
-		<h1>Poulin Wine Cellar!</h1>
+		<h1>Poulin Wine Cellar</h1>
 		<button id="getVineyards">Get Vineyards</button>
 		
 		<div id="vineyards"></div>
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.0/jquery-ui.min.js"></script>
+		<script src="/js/lib/prefix-free.js" type="text/javascript"></script>
 		<script src="/js/main.js" type="text/javascript"></script>
 	</body>
 </html>

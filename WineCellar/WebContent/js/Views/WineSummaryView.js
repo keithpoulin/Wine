c55ba53ca76768588cebb40175d0dfaf89be0ad1
Neil Poulin <neil.j.poulin@gmail.com>
2012-12-19 10:01:43 -0700

    added #ref and IDs to each wineSummray element.

--- WineCellar/WebContent/js/Views/WineSummaryView.js
+++ WineCellar/WebContent/js/Views/WineSummaryView.js
@@ -1,8 +1,12 @@
 var WineSummaryView = Backbone.View.extend({
 	tagName: "li",
+//	id: this.model.get("wineId"),
 	template: templates.wineSummary,
 	initialize: function(){
-		
+		this.attributes.id = "wineSummary" + this.model.get("wineId");
+		for (var key in this.attributes){
+			this.$el.attr(key, this.attributes[key]);
+		}
 	},
 	render: function(){
 		this.$el.html(this.template(this.model.toJSON()));
